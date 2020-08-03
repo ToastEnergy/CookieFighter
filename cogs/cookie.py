dd14fdb97d59cd2a8d174ab2406f3748b728f0fd
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-03 14:49:43 +0200

    fixed a thing

--- cogs/cookie.py
+++ cogs/cookie.py
@@ -52,7 +52,7 @@ class Cookie(commands.Cog):
       emb.description = "Nobody ate the cookie!"
       try:
         await msg.edit(embed = emb)
-        await msg.remove_reactions(self.bot.cookie, ctx.guild.me)
+        await msg.remove_reaction(self.bot.cookie, ctx.guild.me)
       except:
         emb.description = "The original message got deleted, I can't end the game!"
         await ctx.send(embed = emb)
