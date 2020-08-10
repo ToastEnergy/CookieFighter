2d7711539461bc6090f7ba39df9eb2606df2edb4
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-10 18:42:22 +0200

    oof

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+import asyncio
+
+load_dotenv(dotenv_path=".env")
 
 bot = commands.AutoShardedBot(command_prefix = commands.when_mentioned_or("c/"), case_insensitive = True, description = "Fight your friends and be the first to catch the cookie!")
 bot.cookie = "<:mc_cookie:726184620164382741>"
@@ -12,6 +15,8 @@ bot.launchtime = datetime.now()
 bot.remove_command("help")
 bot.load_extension("jishaku")
 
+dblpy = dbl.DBLClient(self.bot, str(os.environ.get("topgg")))
+
 @bot.event
 async def on_ready():
     print("ready as", bot.user)
@@ -19,10 +24,15 @@ async def on_ready():
     cmd = bot.get_command("jishaku")
     cmd.hidden = True
 
+def voted():
+    def predicate(ctx):
+        check = asyncio.run(dblpy.get_user_vote(ctx.author.id))
+        return check == True
+    return commands.check(predicate)
+
 for a in os.listdir("./cogs"):
     if a.endswith(".py"):
         bot.load_extension(f"cogs.{a[:-3]}")
 
-load_dotenv(dotenv_path=".env")
 token = os.environ.get("token")
 bot.run(token)
