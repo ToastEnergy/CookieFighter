8af6a0927bf79f03a69800f1653d5b534e667785
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-21 12:10:32 +0200

    custom prefixes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 import asyncio
+import sqlite3
 
 load_dotenv(dotenv_path=".env")
 
@@ -11,7 +12,26 @@ os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
 os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
 os.environ["JISHAKU_HIDE"] = "True"
 
-bot = commands.AutoShardedBot(command_prefix = commands.when_mentioned_or("c/"), case_insensitive = True, description = "Fight your friends and be the first to catch the cookie!")
+def get_prefix(bot, message):
+
+  if message.guild is None:
+    prefix = commands.when_mentioned_or("c/ ", "c/")(bot, message)
+  
+  else:
+    with sqlite3.connect("data/db.db") as db:
+        data = db.execute(f"SELECT * from prefixes where guild = {message.guild.id}")
+        data = data.fetchall()
+
+    if len(data) >= 1:
+      new_prefix = str(data[0][1])
+      prefix = commands.when_mentioned_or(f"{new_prefix} ", new_prefix)(bot, message)
+
+    else:
+      prefix = commands.when_mentioned_or("c/ ", "c/")(bot, message)
+
+  return prefix
+
+bot = commands.AutoShardedBot(command_prefix = get_prefix, case_insensitive = True, description = "Fight your friends and be the first to catch the cookie!")
 bot.cookie = "<:mc_cookie:726184620164382741>"
 bot.milk = "<:mc_milk:726522958847279174>"
 bot.colour = 0xd8ad6a
