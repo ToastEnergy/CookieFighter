d4e0b364d518f3a5c441e6fd9875946ac4f70508
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-28 11:25:07 +0200

    clock

--- cogs/cookie.py
+++ cogs/cookie.py
@@ -210,7 +210,7 @@ class Cookie(commands.Cog):
           u = self.bot.get_user(int(lb[data]["user"]))
           if u:
             counter += 1
-            res += f"\n**{counter}.** `{str(u)}` - **{lb[data]['time']} 🕐**"
+            res += f"\n**{counter}.** `{str(u)}` - **{lb[data]['time']}s {self.bot.clock}**"
         
     else:
       stats = {}
