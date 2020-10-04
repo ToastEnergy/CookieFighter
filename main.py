be25b7842468f84434c3560c37c13a98a5ed89b0
ssebastianoo <sebastianogirotto05@gmail.com>
2020-10-04 13:38:54 +0200

    Ididi

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def get_prefix(bot, message):
 
   return prefix
 
-intents = discord.Intents()
+intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.AutoShardedBot(command_prefix = get_prefix, case_insensitive = True, description = "Fight your friends and be the first to catch the cookie!", intents = intents)
