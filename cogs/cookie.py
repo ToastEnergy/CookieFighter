31af796097ca5271e490e2fa40fe28b42e226f8e
ssebastianoo <sebastianogirotto05@gmail.com>
2020-10-30 23:46:57 +0100

    oof2 g

--- cogs/cookie.py
+++ cogs/cookie.py
@@ -247,7 +247,7 @@ class Cookie(commands.Cog):
             if u:
               counter += 1
               user = str(u).replace("`", "")
-              res += f"\n**{counter}.** `{discord.utils.escape_markdown(str(user))}` - **{lb[data]['time']}s {self.bot.clock}**"
+              res += f"\n**{counter}.** `{user}` - **{lb[data]['time']}s {self.bot.clock}**"
         
     else:
       stats = {}
@@ -281,7 +281,7 @@ class Cookie(commands.Cog):
             if u:
               counter += 1
               user = str(u).replace("`", "")
-              res += f"\n**{counter}.** `{discord.utils.escape_markdown(str(user))}` - **{stats[str(data)]} {self.bot.cookie}**"
+              res += f"\n**{counter}.** `{user}` - **{stats[str(data)]} {self.bot.cookie}**"
 
     emb = discord.Embed(description = res, colour = self.bot.colour)
     emb.set_author(name = "Global Leaderboard", icon_url = "https://cookiefighter.github.io/cdn/cookie_gif.gif")
